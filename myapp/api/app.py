f6c6c77d60127de7bee3c5825d38ff7d690f257c
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-07-17 02:37:08 +0100

    made changes to models

--- myapp/api/app.py
+++ myapp/api/app.py
@@ -1,7 +1,21 @@
 from flask import Blueprint
+from flask.views import MethodView
 
-api = Blueprint('api', __name__, url_prefix='/api',
-                template_folder='templates')
+api = Blueprint('api', __name__, template_folder='templates')
+
+
+class User(MethodView):
+    def get(self):
+        return 'returned a user'
+
+    def post(self):
+        return 'Created an user'
+
+    def put(self):
+        return 'Modified a user'
+
+    def delete(self):
+        return 'Deleted an user'
 
 
 @api.route('/')
--- myapp/models.py
+++ myapp/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     join_date = db.Column(
         db.DateTime, default=datetime.utcnow, nullable=False)
     userprofile = db.relationship('Userprofile', backref='user', uselist=False)
-    # posts = db.relationship('Post', backref='user_post', lazy=True)
+    posts = db.relationship('Post', backref='userposts', lazy=True)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -53,53 +53,57 @@ class Friend(db.Model):
         db.DateTime, default=datetime.utcnow, nullable=False)
 
 
-# class Post(db.Model):
-#     __tablename__ = "post"
-#     # __table_args__ = {'extend_existing': True}
-#     id = db.Column(db.Integer, primary_key=True)
-#     post_body = db.Column(db.String(500), nullable=False)
-#     date_created = db.Column(
-#         db.DateTime, default=datetime.utcnow, nullable=False)
-#     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-#     comments = db.relationship(
-#         'Comment', backref='postcomments', lazy='dynamic')
-#     post_image = db.relationship(
-#         'PostImage', backref='postimage', lazy='dynamic')
+class Message(db.Model):
+    __tablename__ = "message"
+    id = db.Column(db.Integer, primary_key=True)
+    sent_from = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    sent_to = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.String(500), nullable=False)
+    user = db.relationship('User', foreign_keys=[sent_from])
+    friend = db.relationship('User', foreign_keys=[sent_to])
+    date_sent = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
 
 
-# class PostImage(db.Model):
-#     __tablename__ = "postimage"
-#     id = db.Column(db.Integer, primary_key=True)
-#     path = db.Column(db.String(500), nullable=False)
-#     post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+class Post(db.Model):
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    post_body = db.Column(db.String(500), nullable=False)
+    post_image = db.Column(db.String(500), nullable=True)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
+    comments = db.relationship(
+        'Comment', backref='postcomments', lazy='dynamic')
 
 
-# class Comment(db.Model):
-#     __tablename__ = "comment"
-#     id = db.Column(db.Integer, primary_key=True)
-#     comment_body = db.Column(db.String, nullable=False)
-#     date_created = db.Column(
-#         db.DateTime, default=datetime.utcnow, nullabe=False)
-#     user_id = db.Column(db.Integer, db.ForiegnKey('user.id'), nullable=False)
-#     post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+class Comment(db.Model):
+    __tablename__ = "comment"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    Comment_like = db.Column(db.Integer, default=0, nullable=False)
+    comment_body = db.Column(db.String, nullable=False)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
 
 
-# class PostLike(db.Model):
-#     __tablename__ = "postlike"
-#     like_id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
-#     post_id = db.Column(db.Integer, db.Foreignkey('post.id'), nullable=False)
-#     numberof_like = db.Column(db.Integer, nullable=False)
-#     date_created = db.Column(
-#         db.DateTime, default=datetime.utcnow, nullabe=False)
+class PostLike(db.Model):
+    __tablename__ = "postlike"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    numberof_like = db.Column(db.Integer, default=0, nullable=False)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
 
 
-# class CommentLike(db.Model):
-#     __tablename__ = "commentlike"
-#     like_id = db.Column(db.Integer, primary_key=True)
-#     user_id = db.Column(db.Integer, db.Foreignkey('user.id'), nullable=False)
-#     comment_id = db.Column(
-#         db.Integer, db.Foreignkey('post.id'), nullable=False)
-#     numberof_like = db.Column(db.Integer, nullable=False)
-#     date_created = db.Column(
-#         db.DateTime, default=datetime.utcnow, nullabe=False)
+class CommentLike(db.Model):
+    __tablename__ = "commentlike"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    comment_id = db.Column(
+        db.Integer, db.ForeignKey('post.id'), nullable=False)
+    numberof_like = db.Column(db.Integer, default=0, nullable=False)
+    date_created = db.Column(
+        db.DateTime, default=datetime.utcnow, nullable=False)
